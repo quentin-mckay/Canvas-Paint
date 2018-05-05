971b59fbe38ea33d600085b10d999ebd240f326b
Quentin McKay <quentinmckay1@gmail.com>
2018-05-05 18:05:44 +1000

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function draw(e) {
     // lastY = e.offsetY
     hue += 1
 
-    if (ctx.lineWidth >= 150 || ctx.lineWidth <=1) {
+    if (ctx.lineWidth >= 500 || ctx.lineWidth <=1) {
         direction = !direction
     }
 
